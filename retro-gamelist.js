d0097362c5f662c4c871e065ea95cd1a99189553
Andres Quinones <aquinones340@hotmail.com>
2019-02-02 18:47:35 -0500

    Updated to store extension and path

--- retro-gamelist.js
+++ retro-gamelist.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 
 
-function RetroGame(name, rom, savefiles)  {
+function RetroGame(name, extension, absgamepath, savefiles)  {
 	this.name = name
-	this.rom = rom
+	this.extension = extension
+	this.absgamepath = absgamepath
 	this.savefiles = savefiles
 	this.available = true
 	this.checkedtime = new Date()
@@ -13,8 +14,12 @@ function RetroGame(name, rom, savefiles)  {
 	     this.name = name;
     }
 
-    this.setRom = function(rom) {
-	    this.rom = rom;
+	this.setExt= function(ext) {
+	     this.extension = ext;
+    }
+
+    this.setGamePath = function(gamepath) {
+	    this.absgamepath = gamepath;
     }
 
     this.setSavefiles = function(s) {
@@ -25,8 +30,12 @@ function RetroGame(name, rom, savefiles)  {
 	    return this.name;
     }
 
-    this.getRom = function() {
-	    return this.rom;
+	this.getExt= function() {
+	     return this.extension;
+    }
+
+    this.getGamePath = function() {
+	    return this.absgamepath;
     }
 
     this.getSavefiles = function() {
@@ -79,8 +88,8 @@ function RetroGames() {
 			this.retrogames[game.getName()] = game;
 	}
 
-	this.addGameRaw = function (name, rom, savefiles) {
-		var game1 = new RetroGame(name, rom, savefiles);
+	this.addGameRaw = function (name, extension, gamepath, savefiles) {
+		var game1 = new RetroGame(name, extension, gamepath, savefiles);
 
 		if ( !(game1.getName() in this.retrogames) )
 			this.retrogames[game1.getName()] = game1;
@@ -90,15 +99,61 @@ function RetroGames() {
 		delete this.retrogames[key];
 	}
 
-	this.addFilesFromFolder = function (folder) {
-		var result = fs.readdirSync(folder);
-		for (var i = 0; i < result.length; i++) {
-			gamename = result[i].replace(/\.[^/.]+$/, "");
-			gamerom = folder + "/" + result[i];
-			gamesave = "";
-			this.addGameRaw(gamename, gamerom, gamesave);
+	this.getFilename = function (filepath) {
+		file = filepath.split('\\').pop().split('/').pop();
+		var lastIndex = file.lastIndexOf(".");
+		if (lastIndex < 1) { 
+			return "";
 		}
+		return file.substr(0, lastIndex);
 	}
+
+    this.getExtension = function (filepath) {
+    	file = filepath.split('\\').pop().split('/').pop();
+		var lastIndex = file.lastIndexOf(".");
+		if (lastIndex < 1) { 
+			return "";
+		}
+		return file.substr(lastIndex + 1);
+    }
+
+    this.isRom = function (extension) {
+    	ext = extension.toLowerCase();
+    	if ( ext == "gba" ||
+    	     ext == "gb" ||
+    	     ext == "gbc" ||
+    	     ext == "nds" ||
+    	     ext == "nes" || 
+    	     ext == "n64" ||
+    	     ext == "sfc" ||
+    	     ext == "smc" ||
+    	     ext == "iso" ||
+    	     ext == "cso" ||
+    	     ext == "bin"  )
+    		return true;
+    	return false;
+    }
+
+    this.addFilesFromFolder = function (folder) {
+    	var result = fs.readdirSync(folder, {
+			withFileTypes: "true"
+		});
+		for (var i = 0; i < result.length; i++) {
+			var currentItem = result[i];
+			if ( currentItem.isDirectory() )
+				this.addFilesFromFolder(folder+"/"+currentItem.name);
+			if ( currentItem.isFile() ) {
+				var gameext = this.getExtension(currentItem.name);
+				if ( this.isRom(gameext) ) {
+					var gamename = this.getFilename(currentItem.name);
+			   	 	var absgamepath = folder + "/" + currentItem.name;
+			   		var gamesave = "";
+			    	this.addGameRaw(gamename, gameext, absgamepath, gamesave);
+				}
+
+			}
+		}
+    }
 
 	this.loadFromFile = function () {
 		var contents = fs.readFileSync("games.txt", "utf8");
@@ -109,10 +164,11 @@ function RetroGames() {
 
 		for (var i = 0; i < splitcontents.length; i++) {
 			splitcontents[i] = splitcontents[i].split("|");
-		    var tempGame = new RetroGame(splitcontents[i][0], splitcontents[i][1], splitcontents[i][2].split(":"));
+		    var tempGame = new RetroGame(splitcontents[i][0], splitcontents[i][1], splitcontents[i][2], splitcontents[i][3].split(":"));
 	    	this.addGame(tempGame);
 		}
 	}
+
 
 	this.saveToFile = function () {
 
@@ -120,7 +176,7 @@ function RetroGames() {
 		var allkeys = Object.keys(this.retrogames)
 		for (var i = 0; i < allkeys.length; i++) {
 			var tempgame = this.retrogames[allkeys[i]];
-			data += tempgame.getName() + "|" + tempgame.getRom() + "|";
+			data += tempgame.getName() + "|" + tempgame.getExt() + "|" + tempgame.getGamePath() + "|";
 			var tempSavefiles = tempgame.getSavefiles();
 			for (var j = 0; j < tempSavefiles.length; j++) {
 				data+= tempSavefiles[j];
